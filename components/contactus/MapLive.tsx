4731774acfb6393d46dca3b1e56abe4875dea71f
vannarith7 <none@gmail.com>
2024-08-20 10:14:52 +0700

    fixed active map live location

--- components/contactus/MapLive.tsx
+++ components/contactus/MapLive.tsx
@@ -1,43 +1,47 @@
 'use client'
-import React, { useCallback, useState } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { title } from 'process';
+import { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
 const MapLive = () => {
-const options = {
-  mapId: "f870b9ae79c58b9b",
-  title:"you are here"
-}
-const center = {
-  lat: 11.544067280876929,
-  lng: 104.93804282897477,
-  title: "you are here"
-};
-  const {isLoaded} = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyAm5nGiiONWGGGm6eDdtfzl_CgGWgN3oDo",
-  })
+  const options = {
+    mapId: "f870b9ae79c58b9b",
+    title: "you are here"
+  }
+  const center = {
+    lat: 11.544067280876929,
+    lng: 104.93804282897477
+  };
+  const containerStyle = {
+    height: '600px',
+    width: '100%',
+  };
 
+  const [location, setLocation] = useState(center);
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      });
+    }
+  }, []);
 
-  return isLoaded? (
-    <GoogleMap
-    options={options}
-      mapContainerStyle={{
-        height: '600px',
-        width: '100%',
-      }}
-      center={center}
-      zoom={15}
-      
-
-    >
-     <></>
-    </GoogleMap>
-  ) : <></>
-
-  
   return (
-    <main className={`w-[60%] mt-12`}>MapLive</main>
-  )
+    <LoadScript googleMapsApiKey="AIzaSyAm5nGiiONWGGGm6eDdtfzl_CgGWgN3oDo">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={location}
+        zoom={15}
+        options={options}
+      >
+        <Marker position={location} />
+      </GoogleMap>
+    </LoadScript>
+  );
+
 }
 
 export default MapLive
